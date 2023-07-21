721c330373a69bb6e56b803ac6ba5bda00bf4377
Vazgen <vouqreel@gmail.com>
2023-07-22 04:02:16 +0500

    adding all changed files

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 import CustomButton from "./CustomButton";
 
-export { CustomButton };
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+
+export { CustomButton, ColorPicker, FilePicker, Tab };
