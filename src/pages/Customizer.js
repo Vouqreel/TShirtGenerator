2da96b97e63b0e26bacd39152bf4ec477ff9b760
Vazgen <vouqreel@gmail.com>
2023-07-22 04:27:29 +0500

    build -v and new save btn

--- src/pages/Customizer.js
+++ src/pages/Customizer.js
@@ -36,33 +36,6 @@ const Customizer = () => {
 		}
 	};
 
-	const handleSubmit = async (type) => {
-		if (!prompt) return alert("Please enter a prompt");
-
-		try {
-			setGeneratingImg(true);
-
-			const response = await fetch("http://localhost:8080/api/v1/dalle", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({
-					prompt,
-				}),
-			});
-
-			const data = await response.json();
-
-			handleDecals(type, `data:image/png;base64,${data.photo}`);
-		} catch (error) {
-			alert(error);
-		} finally {
-			setGeneratingImg(false);
-			setActiveEditorTab("");
-		}
-	};
-
 	const handleDecals = (type, result) => {
 		const decalType = DecalTypes[type];
 
@@ -137,6 +110,9 @@ const Customizer = () => {
 								handleClick={() => handleActiveFilterTab(tab.name)}
 							/>
 						))}
+						<button className="download-btn" onClick={downloadCanvasToImage}>
+							<img src={download} alt="download_image" className="w-3/5 h-3/5 object-contain" />
+						</button>
 					</motion.div>
 				</>
 			)}
